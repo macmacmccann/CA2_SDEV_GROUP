de1254ac27e1aec1378320bbe8c4af19d67a68c0
markmccann98@hotmail.com <markmccann98@hotmail.com>
2023-11-18 16:15:04 +0000

    Error corrections,html strucutre,url pattern testing,js

--- productsproject/productsproject/urls.py
+++ productsproject/productsproject/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     
    path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('shop/', include('shop.urls')),
+    #path('shop/', include('shop.urls')),
     path('custom-accounts/', include('accounts.urls')),  # Your custom account URLs
     path('accounts/', include('django.contrib.auth.urls')),
     #path('accounts/',include('accounts.urls')),
